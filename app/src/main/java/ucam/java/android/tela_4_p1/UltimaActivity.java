1dc98f50eb19f114c1fdbe8e10c8f2a6ef73409d
RenatoAreas <rsareas93@gmail.com>
2016-10-18 23:59:06 -0200

    Trabalho de p1 Tela 4 FINAL dia 18/10/2016 as 23:58

---
+++ app/src/main/java/ucam/java/android/tela_4_p1/UltimaActivity.java
@@ -0,0 +1,13 @@
+package ucam.java.android.tela_4_p1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UltimaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ultima);
+    }
+}
